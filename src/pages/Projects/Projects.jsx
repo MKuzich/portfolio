45cbb7115b99f32886070570cc8f723bfa445e34
Mykhailo Kuzich <timmkedth@gmail.com>
2023-01-29 11:32:25 +0200

    added some projects info

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,40 +1,39 @@
-import { Container, Grid } from '@mui/material';
+import { Container, Grid, Button, Box } from '@mui/material';
+import { projects } from 'data/projects';
 
 const Projects = () => {
+  const handleOpen = () => {};
+
   return (
     <section>
       <Container>
-        {/* <div>filter</div>
-        <h1>My projects</h1> */}
-        <Grid container spacing={4}></Grid>
+        <Grid container spacing={4}>
+          {projects.map(({ name, poster }) => (
+            <Grid key={name} item xxs={4}>
+              <Button
+                onClick={handleOpen}
+                sx={{
+                  flexShrink: 0,
+                }}
+              >
+                <Box
+                  sx={{
+                    backgroundImage: `url(${poster})`,
+                    height: '300px',
+                    filter: 'grayscale(1)',
+                    '&': { transition: 'filter 500ms' },
+                    '&:hover': {
+                      filter: 'grayscale(0)',
+                    },
+                  }}
+                />
+              </Button>
+              ;
+            </Grid>
+          ))}
+        </Grid>
       </Container>
     </section>
   );
 };
 export default Projects;
-
-// <Button
-//   key={'btn' + speciality}
-//   onClick={() => handleOpen(image, speciality)}
-//   sx={{
-//     flexShrink: 0,
-//     position: 'absolute',
-//     right: 0,
-//     top: 0,
-//     zIndex: 2,
-//   }}
-// >
-//   <Box
-//     src={image}
-//     alt={speciality}
-//     component="img"
-//     sx={{
-//       height: '300px',
-//       filter: 'grayscale(1)',
-//       '&': { transition: 'filter 500ms' },
-//       '&:hover': {
-//         filter: 'grayscale(0)',
-//       },
-//     }}
-//   />
-// </Button>;
