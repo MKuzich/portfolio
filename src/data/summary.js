0c4147d641653ec2e21cdb12e6b88d8ab420778d
MKuzich <97763883+MKuzich@users.noreply.github.com>
2023-02-02 11:05:40 +0000

    Deploying to gh-pages from @ MKuzich/portfolio@12b21c9d8487f4cc98d6bb11b0a60c7f457db94f 🚀

--- src/data/summary.js
+++
@@ -1,5 +0,0 @@
-export const summary = [
-  'Hi, my name is Mykhailo Kuzich . I am an open-minded Full Stack developer with strong analytical thinking, good in learning new technologies looking for a full-time position.',
-  'My native language - is Ukrainian, and my secondary - is English at an intermediate level.',
-  'I have vast experience in managing a big team, problem-solving, making important technical decisions, analyzing and working with large volumes of technical documentation, planning my workflow and the workflow for my team.',
-];
